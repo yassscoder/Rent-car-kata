85255fc93584fad928862bf4b1f4478ba97d2057
yassscoder <icanmakeitonmyown@gmail.com>
2024-03-26 11:15:04 +0100

    green: throws exception when car is not found

--- src/main/java/org/example/services/RentalService.java
+++ src/main/java/org/example/services/RentalService.java
@@ -1,19 +1,25 @@
 package org.example.services;
 
 import org.example.models.Rental;
+import org.example.repositories.CarRepository;
 import org.example.repositories.UserRepository;
 
 
 public class RentalService {
+    CarRepository carRepository;
     UserRepository userRepository;
 
-    public RentalService(UserRepository userRepository) {
+    public RentalService(UserRepository userRepository, CarRepository carRepository) {
         this.userRepository = userRepository;
+        this.carRepository = carRepository;
     }
 
     public Rental saveRental(Long userId, Long carId) {
         if (userRepository.findById(userId).isEmpty()) {
             throw new NullPointerException("user not found");
+        }
+        if (carRepository.findById(carId).isEmpty()) {
+            throw new NullPointerException("car not found");
         }
         return new Rental();
     }
