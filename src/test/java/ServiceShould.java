661c454ea589f1cfd268df22779faa6200211625
yassscoder <icanmakeitonmyown@gmail.com>
2024-03-26 10:26:43 +0100

    test: improve test

--- src/test/java/ServiceShould.java
+++ src/test/java/ServiceShould.java
@@ -6,9 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 
 public class ServiceShould {
@@ -18,13 +21,14 @@ public class ServiceShould {
     RentalService rentalService;
 
     @BeforeEach
-    void initService(){
+    void initService() {
         openMocks(this);
     }
 
     @Test
     @DisplayName("not save rental, user not found")
-    void not_save_rental_user_not_found(){
+    void not_save_rental_user_not_found() {
+        when(userRepository.findById(1L)).thenReturn(Optional.empty());
         RuntimeException exception = assertThrows(RuntimeException.class, () -> rentalService.saveRental(1L, 2L));
         assertEquals("user not found", exception.getMessage());
     }
